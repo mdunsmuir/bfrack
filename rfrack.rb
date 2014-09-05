6ba99d599cddc40ab1bebd1b09862419735a76c6
Michael Dunsmuir <mjdunsmuir@gmail.com>
2014-09-04 17:44:24 -0700

    some changes to the ruby one i guess

--- rfrack.rb
+++ rfrack.rb
@@ -1,93 +1,102 @@
+# > increment the data pointer (to point to the next cell to the right).
+# < decrement the data pointer (to point to the next cell to the left).
+# + increment (increase by one) the byte at the data pointer.
+# - decrement (decrease by one) the byte at the data pointer.
+# . output the byte at the data pointer.
+# , accept one byte of input, storing its value in the byte at the data pointer.
+# [ if the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching ] command.
+# ] if the byte at the data pointer is nonzero, then instead of moving the instruction pointer forward to the next command, jump it back to the command after the matching [ command.
+
+prog = IO.read(ARGV.shift)
+
 CMDS = {
-  "." => "putc(tape[ptr].chr)",
+  ">" => "ptr += 1",
+  "<" => "ptr -= 1",
+  "+" => "tape[ptr] ||= 0; tape[ptr] += 1",
+  "-" => "tape[ptr] ||= 0; tape[ptr] -= 1",
+  "." => "tape[ptr] ||= 0; putc(tape[ptr].chr)",
   "," => "tape[ptr] = $stdin.getbyte"
 }
 
-def block(prog, dep = 0)
+def block(prog)
   comp = []
-  indent = "  " * dep
 
-  mods = {}
-  mods.default = 0
-
-  ptr = 0
-
-  rotate_mods = lambda{
-    unless mods.size == 0
-      mods.each{ |imod, mod|
-        next if mod == 0
-        str = ["tape["]
-        str << "ptr"
-        if imod > 0
-          str << " + #{imod}"
-        elsif imod < 0
-          str << " - #{imod.abs}"
-        end
-        str << "] "
-        if mod > 0
-          str << "+= "
-        else
-          str << "-= "
-        end
-        str << mod.abs.to_s
-        comp << indent + str.join
-      }
-      mods = {}
-      mods.default = 0
-    end
-    if ptr > 0
-      comp << indent + "ptr += #{ptr}"
-    elsif ptr < 0
-      comp << indent + "ptr -= #{ptr.abs}"
-    end
-    ptr = 0
-  }
+  cmd = nil
+  amt = 0
 
   i = 0
   while i < prog.length
     c = prog[i]
-    if c == '>'
-      ptr += 1
-    elsif c == '<'
-      ptr -= 1
-    elsif c == '+'
-      mods[ptr] += 1
-    elsif c == '-'
-      mods[ptr] -= 1
-    elsif c == '['
-      stack = 1
-      j = i + 1
-      while stack > 0
-        if j >= prog.length
-          raise "bad brackets"
-        elsif prog[j] == '['
-          stack += 1
-        elsif prog[j] == ']'
-          stack -= 1
+    if c == '>' or c == '<' or c == '+' or c == '-'
+      if cmd == c
+        if c == '>' || c == '+'
+          amt += 1
+        else c == '<' || c == '-'
+          amt -= 1
         end
-        j += 1
+      else
+        if cmd == '>' or cmd == '<'
+          comp << "ptr += #{amt}"
+        elsif cmd == '-' or cmd == '+'
+          comp << "tape[ptr] ||= 0; tape[ptr] += #{amt}"
+        end
+        cmd = c
+        if cmd == '>' or cmd == '+'
+          amt = 1
+        else
+          amt = -1
+        end
       end
-      rotate_mods.call
-      comp << indent + "ptr += #{ptr}" unless ptr == 0
-      ptr = 0
-      comp << indent + "while tape[ptr] != 0"
-      str = block(prog[(i + 1)...(j - 1)], dep + 1)
-      comp << str
-      comp << indent + "end"
-      i = j - 1
-    elsif c != ']' and CMDS[c]
-      rotate_mods.call
-      comp << indent + CMDS[c]
+    else
+      if cmd and amt != 0
+        if cmd == '>' or cmd == '<'
+          comp << "ptr += #{amt}"
+        else
+          comp << "tape[ptr] ||= 0; tape[ptr] += #{amt}"
+        end
+        cmd = nil
+        amt = 0
+      end
+
+      if c == '['
+        stack = 1
+        j = i + 1
+        while stack > 0
+          if j >= prog.length
+            raise "bad brackets"
+          elsif prog[j] == '['
+            stack += 1
+          elsif prog[j] == ']'
+            stack -= 1
+          end
+          j += 1
+        end
+        comp << "while tape[ptr] and tape[ptr] != 0"
+        comp << block(prog[(i + 1)...(j - 1)])
+        comp << "end"
+        i = j - 1
+      elsif c != ']'
+        comp << CMDS[c]
+      end
     end
     i += 1
   end
 
-  rotate_mods.call
+  if cmd and amt != 0
+    if cmd == '>' or cmd == '<'
+      comp << "ptr += #{amt}"
+    else
+      comp << "tape[ptr] ||= 0; tape[ptr] += #{amt}"
+    end
+  end
+
   return comp.join("\n")
 
 end
 
-prog = IO.read(ARGV.shift)
+#tape = {}
+#ptr = 0
 
-File.open("bfprog.rb", 'w'){ |f| f.write("tape = {}\ntape.default = 0\nptr = 0\n" + block(prog)) }
+File.open("bfprog.rb", 'w'){ |f| f.write("tape = {}; ptr = 0\n" + block(prog)) }
 
+#eval(block(prog))
