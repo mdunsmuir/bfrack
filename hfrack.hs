0bd8fbe1a6fa16bcde6877fcac503ed37d409e00
Michael Dunsmuir <mdunsmuir@3tier.com>
2014-09-08 18:05:13 -0700

    made hfrack more monad-y

--- hfrack.hs
+++ hfrack.hs
@@ -1,120 +1,113 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 
 import System.Environment
+import System.IO
+import Control.Exception
+import Data.List
+import Data.Typeable
+import Control.Monad.Writer.Lazy
+import Control.Monad.State.Lazy
+import Control.Monad.ST
+import Data.Word
 import qualified Data.ByteString as B
 import qualified Data.Vector as V
 import qualified Data.Vector.Mutable as MV
-import Control.Exception
-import Data.Typeable
-import Control.Monad.ST
-import Data.Word
-
-data MismatchedBracesException = MismatchedBracesException deriving (Show, Typeable)
-instance Exception MismatchedBracesException
-
-data MapIndex = Index Int | None deriving Show
 
 initSize = 30000
 
-main = do
-  args <- getArgs
-  if length args /= 1
-    then putStrLn "gotta give a filename"
-    else do prog <- readFile $ head args
-            stringBF prog
-
-stringBF :: String -> IO ()
-stringBF prog = do
-  let vProg = V.fromList $ map charToWord prog
-  brainfuck vProg
-
-brainfuck :: V.Vector Word8 -> IO ()
-brainfuck prog = do
-  tape <- MV.replicate initSize 0
-  progMap <- mapProg prog
-  runBF tape prog 0 0 progMap
-
-runBF tape prog iTape iProg progMap
-  | iProg == V.length prog = return () 
-  | otherwise = case prog V.! iProg of
-
-                  60 -> runBF tape prog (iTape - 1) (iProg + 1) progMap -- <
-                  62 -> runBF tape prog (iTape + 1) (iProg + 1) progMap -- > 
-
-                  43 -> tapeOper (+1) -- +
-                  45 -> let sub y x = x - y
-                        in  tapeOper (sub 1) -- -
-
-                  46 -> do byte <- MV.read tape iTape -- .
-                           putChar $ wordToChar byte
-                           runBF tape prog iTape (iProg + 1) progMap
-
-                  91 -> do byte <- MV.read tape iTape
-                           case byte of
-                             0 -> let newIProg = case progMap V.! iProg of Index i -> i
-                                  in  runBF tape prog iTape newIProg progMap
-                             _ -> runBF tape prog iTape (iProg + 1) progMap
-
-                  93 -> do byte <- MV.read tape iTape
-                           case byte of
-                             0 -> runBF tape prog iTape (iProg + 1) progMap
-                             _ -> let newIProg = case progMap V.! iProg of Index i -> i
-                                  in  runBF tape prog iTape newIProg progMap
-
-                  _ -> runBF tape prog iTape (iProg + 1) progMap
-                  
-  where tapeOper oper = do curVal <- MV.read tape iTape
-                           MV.write tape iTape (oper curVal)
-                           runBF tape prog iTape (iProg + 1) progMap
-
--- map stuff
+data MismatchedBracesException = MismatchedBracesException deriving (Show, Typeable)
+instance Exception MismatchedBracesException
 
 charToWord :: Char -> Word8
 charToWord = toEnum . fromEnum
 wordToChar :: Word8 -> Char
 wordToChar = toEnum . fromEnum
 
-strToBStr = B.pack . map charToWord
-bStrToStr = map wordToChar . B.unpack
+main = do
+  args <- getArgs
+  if length args /= 1
+    then putStrLn "gotta give a filename"
+    else do prog <- B.readFile $ head args
+            brainfuck prog
+            return ()
 
-getProgMap :: String -> IO [MapIndex]
-getProgMap prog = do
-  let vProg = V.fromList $ map charToWord prog
-  indicesV <- mapProg vProg
-  return $ V.toList indicesV
+brainfuck prog' = do
+  tape <- MV.replicate initSize 0
+  let state = (tape, 0, 0)
+  runStateT runBF state
 
-mapProg :: V.Vector Word8 -> IO (V.Vector MapIndex)
-mapProg prog
-  | nLBrackets /= nRBrackets = throwIO MismatchedBracesException
-  | otherwise = do
-      map' <- MV.replicate (V.length prog) None :: IO (MV.IOVector MapIndex)
-      mapProgBackwards map' prog 0 []
-      mapProgForwards map' 0
-      V.freeze map'
+  where prog = V.fromList $ B.unpack prog'
+        progMap = runST $ mapProg prog
+        jump' = jump progMap
+        runBF = do
+          (_, _, iProg) <- get
+          if iProg == V.length prog then return () else do
+            case prog V.! iProg of
+              60 -> incTape (subtract 1) >> next
+              62 -> incTape (+1) >> next
+              43 -> tapeOper (+1) >> next
+              44 -> acceptByte >> next
+              45 -> tapeOper (subtract 1) >> next
+              46 -> printByte >> next
+              91 -> jump' (== 0)
+              93 -> jump' (/= 0)
+              _  -> next
+            runBF
 
-  where
-    nLBrackets = V.length $ V.filter (91 ==) prog
-    nRBrackets = V.length $ V.filter (93 ==) prog
+getByte = do (tape, iTape, iProg) <- get
+             lift $ MV.read tape iTape
 
-    mapProgBackwards :: MV.IOVector MapIndex -> V.Vector Word8 -> Int -> [MapIndex] -> IO ()
-    mapProgBackwards map' prog iProg bStack
-      | iProg == V.length prog = return ()
-      | progVal == 93 && null bStack = throwIO MismatchedBracesException
+incTape oper = do (tape, iTape, iProg) <- get
+                  put (tape, oper iTape, iProg)
 
-      | otherwise = case progVal of
-                      91 -> mapProgBackwards map' prog (iProg + 1) (Index (iProg + 1) : bStack)
-                      93 -> let (bi:bis) = bStack
-                            in  MV.write map' iProg bi >> 
-                                  mapProgBackwards map' prog (iProg + 1) bis
-                      _ -> mapProgBackwards map' prog (iProg + 1) bStack
+tapeOper oper = do (tape, iTape, iProg) <- get
+                   curVal <- lift $ MV.read tape iTape
+                   lift $ MV.write tape iTape (oper curVal)
 
-      where progVal = prog V.! iProg
+acceptByte = do (tape, iTape, iProg) <- get
+                lift $ hFlush stdout
+                inByte <- lift getChar
+                lift $ MV.write tape iTape (charToWord inByte)
+            
 
-    mapProgForwards :: MV.IOVector MapIndex -> Int -> IO ()
-    mapProgForwards map' iMap
-      | iMap == MV.length map' = return ()
-      | otherwise = do mapVal <- MV.read map' iMap
-                       case mapVal of 
-                         Index i -> MV.write map' (i - 1) (Index (iMap + 1))
-                         _ -> return ()
-                       mapProgForwards map' (iMap + 1)
+printByte = do byte <- getByte
+               lift (putChar (wordToChar byte))
+               lift $ hFlush stdout
+
+jump progMap byteCond = do (tape, iTape, iProg) <- get
+                           byte <- getByte
+                           if byteCond byte 
+                             then put (tape, iTape, progMap V.! iProg)
+                             else next
+
+next = do (tape, iTape, iProg) <- get
+          put (tape, iTape, iProg + 1)
+
+-- functions for building the brace map
+
+type BracePair = (Int, Int)
+data Side = L | R deriving Eq
+data Brace = Brace Side Int deriving Eq
+instance Ord Brace where
+  (Brace _ a) <= (Brace _ b) = a <= b
+
+mapProg :: V.Vector Word8 -> ST s (V.Vector Int)
+mapProg prog = do
+  let bracePairs = getBracePairs $ V.toList prog
+  map' <- MV.replicate (V.length prog) 0
+  forM_ bracePairs $ \(l, r) -> MV.write map' l r >> MV.write map' r l
+  V.freeze map'
+
+getBracePairs :: [Word8] -> [BracePair]
+getBracePairs prog
+  | length lbs /= length rbs = throw MismatchedBracesException
+  | null stk  = pairs
+  | otherwise = throw MismatchedBracesException
+  where lbs = map (Brace L) $ findIndices (91 ==) prog
+        rbs = map (Brace R) $ findIndices (93  ==) prog
+        bs = sort $ lbs ++ rbs
+
+        -- what have I done????
+        (stk, pairs) = runWriter $ foldM bFold [] bs
+        bFold stk b@(Brace L _) = return (b : stk)
+        bFold ((Brace L li):stk) b@(Brace R ri) = tell [(li, ri)] >> return stk
