3775036d20b024a8c24b16eb4ee4ff472dfd68b4
Michael Dunsmuir <mjdunsmuir@gmail.com>
2014-09-09 16:49:58 -0700

    hefunge works

--- hefunge/grid.hs
+++ hefunge/grid.hs
@@ -1,4 +1,4 @@
-module Grid (Grid, replicate, read, write) where
+module Grid (Grid, replicate, fromLists, read, write) where
 
 import Prelude hiding (replicate, read)
 import qualified Prelude as P
@@ -10,6 +10,14 @@ type Grid a = V.Vector (VM.IOVector a)
 
 replicate :: Int -> Int -> a -> IO (Grid a)
 replicate w h a = (liftM V.fromList) $ forM (P.replicate h a) $ VM.replicate w
+
+fromLists :: Int -> Int -> a -> [[a]] -> IO (Grid a)
+fromLists w h d lists = do
+  grid <- replicate w h d
+  forM_ [0..(length lists - 1)] $ \i -> copyList (lists !! i) (grid V.! i)
+  return grid
+  where 
+    copyList l v = forM_ [0..(length l - 1)] $ \i -> VM.write v i (l !! i)
 
 wrapCoordsDo f g x y = let h = V.length g
                            w = VM.length $ V.head g
